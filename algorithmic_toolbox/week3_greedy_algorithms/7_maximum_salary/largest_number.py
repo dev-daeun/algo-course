ab9233ddb8b55e2e4ef8344fec68ca5ccb301e65
kde6260 <kde6260@gmail.com>
2019-06-23 23:12:26 +0900

    week-3 add comments about safe move in 7. Maximum Salary

--- algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ algorithmic_toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -5,17 +5,17 @@ import sys
 
 
 def small_input1():
-    # expected result: 92 39 3 24 23
+    # expected result: 923932423
     return [23, 39, 92, 24, 3]
 
 
 def small_input2():
-    # expected result: 2 21
+    # expected result: 221
     return [21, 2]
 
 
 def small_input3():
-    # expected result: 9 9 6 4 1
+    # expected result: 99641
     return [9, 4, 6, 1, 9]
 
 
@@ -34,6 +34,12 @@ def get_digit(num, i):
 
 
 # time complexity: O(NlogN)
+# Greedy Choice:
+# 이 문제에서 탐욕적인 선택은 숫자의 크기과 무관하게 자릿수가 클수록 무조건 앞에 오게 하는 것이다.
+# Safe Move:
+# 각 숫자들의 i번째 자릿수를 비교했을 때 i번째 자릿수가 가장 큰 숫자를 앞에 둔다.
+# i번째 자릿수가 동일할 경우, (i+1)번째 자릿수를 비교한다.
+# 어떤 숫자의 자릿수가 i보다 작다면, 숫자의 첫번째 자릿수로 돌아가서 비교한다. 즉, 숫자의 자릿수를 순회한다(cycle).
 def largest_number(a):
     digit_len = len(str(max(a)))
     cycled_digits = [itertools.cycle(str(num)) for num in a]
