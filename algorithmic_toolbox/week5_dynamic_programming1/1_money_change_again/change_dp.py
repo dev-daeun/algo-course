7996cc167c5632c7af401bfac16b1ce82ffe9f3f
kde6260 <kde6260@gmail.com>
2019-07-14 15:41:07 +0900

    week-5 solve 1. Change Money Again

---
+++ algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -0,0 +1,68 @@
+# Uses python3
+import sys
+
+
+def small_test():
+    input_data = 34
+    answer = 9  # 34 = 3 + 3 + 4 + 4 + 4 + 4 + 4 + 4 + 4.
+    return input_data, answer
+
+def big_test():
+    input_data = 1003
+    answer = 251  # 1000 = 4 * 250 + 3 * 1
+    return input_data, answer
+
+
+def edge_test():
+    input_data = 1
+    answer = 1
+    return input_data, answer
+
+
+def get_min_share(money):
+    three_rest = money % 3
+    four_rest = money % 4
+
+    if four_rest == 0:
+        return money // 4
+    if three_rest == 0:
+        return money // 3
+    return money
+
+
+def get_change(m):
+    answers = [0]
+    for idx in range(1, m+1):
+        try:
+            min_coins_4 = answers[idx-4]
+        except IndexError:
+            min_coins_4 = 1001
+        
+        try:
+            min_coins_3 = answers[idx-3]
+        except IndexError:
+            min_coins_3 = 1001
+        
+        min_coins_1 = answers[idx-1]
+        m_answer = min(min_coins_1, min_coins_3, min_coins_4) + 1
+        answers.append(m_answer)
+    return answers[m]
+
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
+    # m, expected_answer = small_test()
+    # answer = get_change(m)
+    # print(answer)
+    # assert answer == expected_answer
+
+    # m, expected_answer = big_test()
+    # answer = get_change(m)
+    # assert answer == expected_answer
+
+    # m, expected_answer = edge_test()
+    # answer = get_change(m)
+    # assert answer == expected_answer
+
+
