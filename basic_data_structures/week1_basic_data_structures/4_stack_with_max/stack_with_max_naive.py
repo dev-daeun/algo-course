1d99774df6e883a73a06f04ffdee58d519d04cc0
kde6260 <kde6260@gmail.com>
2019-08-02 20:06:46 +0900

    course-2-week-1 change class and methods names

--- basic_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ basic_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -2,22 +2,6 @@
 import sys
 
 
-class StackWithMax():
-    def __init__(self):
-        self.__stack = []
-
-    def Push(self, a):
-        self.__stack.append(a)
-
-    def Pop(self):
-        assert(len(self.__stack))
-        self.__stack.pop()
-
-    def Max(self):
-        assert(len(self.__stack))
-        return max(self.__stack)
-
-
 class Node(object):
     def __init__(self, val):
         self.val = val
@@ -25,7 +9,7 @@ class Node(object):
         self.next_ = None
 
 
-class DoubleLinkedList(object):
+class StackWithMax(object):
     def __init__(self):
         self.tail = None
 
@@ -33,21 +17,25 @@ class DoubleLinkedList(object):
     def empty(self):
         return not bool(self.tail)
 
-    def add_at_tail(self, node):
+    def push(self, node):
         if self.tail:
             self.tail.next_ = node
             node.prev_ = self.tail
         self.tail = node
 
-    def pop_tail(self):
+    def pop(self):
         if self.tail:
             prev = self.tail.prev_
             prev.next_ = None
             self.tail.prev_ = None
-            del self.tail
             self.tail = prev
-        
-        
+            return None
+        raise Exception()
+    
+    def max(self):
+        pass
+
+
 if __name__ == '__main__':
     stack = StackWithMax()
 
@@ -56,10 +44,10 @@ if __name__ == '__main__':
         query = sys.stdin.readline().split()
 
         if query[0] == "push":
-            stack.Push(int(query[1]))
+            stack.push(int(query[1]))
         elif query[0] == "pop":
-            stack.Pop()
+            stack.pop()
         elif query[0] == "max":
-            print(stack.Max())
+            print(stack.max())
         else:
             assert(0)
