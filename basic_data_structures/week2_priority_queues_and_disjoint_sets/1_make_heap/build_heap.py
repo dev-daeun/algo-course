55df3d98c063f8f87ae54b551412775c1917b453
kde6260 <kde6260@gmail.com>
2019-08-25 21:28:07 +0900

    course-2-week-3 fix code to pass local test but not online test

--- basic_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ basic_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -2,9 +2,11 @@
 
 
 MAX_VALUE = pow(10, 9)
-MIN_VALUE = -1
 
 
+# 0-based indices of the elements to be swapped.
+# left child's index: 2 * i + 1
+# right child's index: 2 * i + 2
 class MinHeap(object):
     def __init__(self, data):
         self.data = data
@@ -14,70 +16,57 @@ class MinHeap(object):
     def size(self):
         return len(self.data)
 
-    @staticmethod
-    def left_idx(i):
-        return 2 * i
-
-    @staticmethod
-    def right_idx(i):
-        return 2 * i + 1
-
     def left(self, i):
         try:
-            return self.data[self.left_idx(i)]
+            return 2 * i + 1, self.data[2*i+1]
         except IndexError:
-            return MAX_VALUE + 1
+            return None, MAX_VALUE + 1
 
     def right(self, i):
         try:
-            return self.data[self.right_idx(i)]
+            return 2 * i + 2, self.data[2*i+2]
         except IndexError:
-            return MAX_VALUE + 1
+            return None, MAX_VALUE + 1
 
     def sift_down(self, i):
         parent = i
         while True:
-            left = self.left(parent)
-            right = self.right(parent)
+            left_idx, left = self.left(parent)
+            right_idx, right = self.right(parent)
             smallest = min(self.data[parent], left, right)
-
-            if smallest == left:
-                left_idx = self.left_idx(parent)
-                self.data[parent], self.data[left_idx] = self.data[left_idx], self.data[parent]
-                self.result.append(f'{parent} {left_idx}')
-                parent = left_idx
-
-            if smallest == right:
-                right_idx = self.right_idx(parent)
-                self.data[parent], self.data[right_idx] = self.data[right_idx], self.data[parent]
-                self.result.append(f'{parent} {right_idx}')
-                parent = right_idx
 
             if smallest == self.data[parent]:
                 break
 
+            if smallest == left:
+                self.data[parent], self.data[left_idx] = self.data[left_idx], self.data[parent]
+                self.result.append('{parent} {left_idx}'.format(parent=parent, left_idx=left_idx))
+                parent = left_idx
+
+            if smallest == right:
+                self.data[parent], self.data[right_idx] = self.data[right_idx], self.data[parent]
+                self.result.append('{parent} {right_idx}'.format(parent=parent, right_idx=right_idx))
+                parent = right_idx
+
     def build_heap(self):
-        mid = (self.size - 1) // 2
-        for x in range(mid, 0, -1):
+        mid = self.size // 2 + 1
+        for x in range(mid, -1, -1):
             self.sift_down(x)
         return self.result
 
 
-def main_for_test():
-    n, elements = open('./tests/04').read().splitlines()
-    elements = list(map(int, elements.split()))
+def test():
+    for num in range(4, 9):
+        n, elements = open('./tests/0{}'.format(num)).read().splitlines()
+        elements = list(map(int, elements.split()))
 
-    m, *swaps = open('./tests/04.a').read().splitlines()
+        m, *swaps = open('./tests/0{}.a'.format(num)).read().splitlines()
 
-    min_heap = MinHeap(data=elements)
-    result = min_heap.build_heap()
+        min_heap = MinHeap(data=elements)
+        result = min_heap.build_heap()
 
-    print(len(result))
-    assert len(result) == m
-    assert result == swaps
-    '''
-    form of result : ['i0 j0', 'i1 j0', ...]
-    '''
+        assert len(result) == int(m)
+        assert result == swaps
 
 
 def main():
@@ -85,12 +74,14 @@ def main():
     data = list(map(int, input().split()))
     assert len(data) == n
 
-    swaps = build_heap(data)
+    min_heap = MinHeap(data=data)
+    swaps = min_heap.build_heap()
 
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for pair in swaps:
+        print(pair)
 
 
 if __name__ == "__main__":
-    main_for_test()
+    test()
+    # main()
