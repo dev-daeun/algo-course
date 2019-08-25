81590fd33136d2be09b6a583c7011f8c6441d14a
kde6260 <kde6260@gmail.com>
2019-08-25 19:05:40 +0900

    course-2-week-3 create answer but does not pass test

--- basic_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ basic_data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,33 +1,78 @@
 # python3
 
 
-def build_heap(data):
-    """Build a heap from ``data`` inplace.
+MAX_VALUE = pow(10, 9)
+MIN_VALUE = -1
 
-    Returns a sequence of swaps performed by the algorithm.
-    """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+
+class MinHeap(object):
+    def __init__(self, data):
+        self.data = data
+        self.result = []
+
+    @property
+    def size(self):
+        return len(self.data)
+
+    @staticmethod
+    def left_idx(i):
+        return 2 * i
+
+    @staticmethod
+    def right_idx(i):
+        return 2 * i + 1
+
+    def left(self, i):
+        try:
+            return self.data[self.left_idx(i)]
+        except IndexError:
+            return MAX_VALUE + 1
+
+    def right(self, i):
+        try:
+            return self.data[self.right_idx(i)]
+        except IndexError:
+            return MAX_VALUE + 1
+
+    def sift_down(self, i):
+        parent = i
+        while True:
+            left = self.left(parent)
+            right = self.right(parent)
+            smallest = min(self.data[parent], left, right)
+
+            if smallest == left:
+                left_idx = self.left_idx(parent)
+                self.data[parent], self.data[left_idx] = self.data[left_idx], self.data[parent]
+                self.result.append(f'{parent} {left_idx}')
+                parent = left_idx
+
+            if smallest == right:
+                right_idx = self.right_idx(parent)
+                self.data[parent], self.data[right_idx] = self.data[right_idx], self.data[parent]
+                self.result.append(f'{parent} {right_idx}')
+                parent = right_idx
+
+            if smallest == self.data[parent]:
+                break
+
+    def build_heap(self):
+        mid = (self.size - 1) // 2
+        for x in range(mid, 0, -1):
+            self.sift_down(x)
+        return self.result
 
 
 def main_for_test():
-    n, elements = open('./tests/04').readlines()
-    elements = elements.split()
+    n, elements = open('./tests/04').read().splitlines()
+    elements = list(map(int, elements.split()))
 
-    result = build_heap(elements)
-    m, swaps = open('./tests/04.a').readlines()
+    m, *swaps = open('./tests/04.a').read().splitlines()
 
+    min_heap = MinHeap(data=elements)
+    result = min_heap.build_heap()
+
+    print(len(result))
     assert len(result) == m
     assert result == swaps
     '''
