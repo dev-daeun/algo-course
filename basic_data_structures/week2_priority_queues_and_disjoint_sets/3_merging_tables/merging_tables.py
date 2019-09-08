614ed6a389b6116c80fb3d2d79dcf15094c71c9c
kde6260 <kde6260@gmail.com>
2019-09-08 22:01:48 +0900

    course-2-week-3 change union method rank-heuristically and change find method iteratively

--- basic_data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ basic_data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -7,25 +7,35 @@ class Database:
         self.row_counts = [0]
         self.row_counts.extend(row_counts)
         self.max_row_counts = max(self.row_counts)
-        self.result = []
         self.parents = [i for i in range(len(self.row_counts) + 1)]
+        self.ranks = [0 for _ in range(len(self.row_counts) + 1)]
 
-    def union(self, dst, src):
-        dst_root = self.find(dst)
-        src_root = self.find(src)
+    def union(self, first, second):
+        first_root = self.find(first)
+        second_root = self.find(second)
+        if first_root == second_root:
+            return self.max_row_counts
 
-        if dst_root == src_root:
-            self.result.append(self.max_row_counts)
-            return
+        if self.ranks[first] > self.ranks[second]:
+            return self.merge(dst=first_root, src=second_root)
 
-        self.parents[src_root] = dst_root
-        self.row_counts[dst_root] += self.row_counts[src_root]
-        self.max_row_counts = max(self.max_row_counts, self.row_counts[dst_root])
-        self.result.append(self.max_row_counts)
+        if self.ranks[first] == self.ranks[second]:
+            self.ranks[first] += 1
+
+        return self.merge(dst=second_root, src=first_root)
+
+    def merge(self, dst, src):
+        self.parents[src] = dst
+        self.row_counts[dst] += self.row_counts[src]
+        self.max_row_counts = max(self.max_row_counts, self.row_counts[dst])
+        return self.max_row_counts
 
     def find(self, table):
-        if table != self.parents[table]:
-            self.parents[table] = self.find(self.parents[table])
+        # path compression
+        p = table
+        while self.parents[p] != p:
+            p = self.parents[p]
+        self.parents[table] = p
         return self.parents[table]
 
 
@@ -34,12 +44,12 @@ def test():
         _, rows, *queries = open(f'./tests/{n}').read().splitlines()
         queries = [q.split() for q in queries]
         db = Database(row_counts=[int(r) for r in rows.split()])
-
+        result = []
         for q in queries:
             dst, src = int(q[0]), int(q[1])
-            db.union(dst, src)
+            result.append(db.union(dst, src))
 
-        assert db.result == list(map(int, open(f'./tests/{n}.a').read().splitlines()))
+        assert result == list(map(int, open(f'./tests/{n}.a').read().splitlines()))
 
 
 def main():
