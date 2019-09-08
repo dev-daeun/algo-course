82d180081a89bd733d73ff693eec1b6b99eed065
kde6260 <kde6260@gmail.com>
2019-09-08 21:15:54 +0900

    course-2-week-3 implement union & find method to pass small testcases find method occurs RecursionError in big testcase

--- basic_data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ basic_data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -2,28 +2,44 @@
 
 
 class Database:
-    def __init__(self, row_counts):
-        self.row_counts = row_counts
-        self.max_row_count = max(row_counts)
-        n_tables = len(row_counts)
-        self.ranks = [1] * n_tables
-        self.parents = list(range(n_tables))
-        self.result = []
 
-    def merge(self, dst, src):
-        pass
+    def __init__(self, row_counts):
+        self.row_counts = [0]
+        self.row_counts.extend(row_counts)
+        self.max_row_counts = max(self.row_counts)
+        self.result = []
+        self.parents = [i for i in range(len(self.row_counts) + 1)]
+
+    def union(self, dst, src):
+        dst_root = self.find(dst)
+        src_root = self.find(src)
+
+        if dst_root == src_root:
+            self.result.append(self.max_row_counts)
+            return
+
+        self.parents[src_root] = dst_root
+        self.row_counts[dst_root] += self.row_counts[src_root]
+        self.max_row_counts = max(self.max_row_counts, self.row_counts[dst_root])
+        self.result.append(self.max_row_counts)
+
+    def find(self, table):
+        if table != self.parents[table]:
+            self.parents[table] = self.find(self.parents[table])
+        return self.parents[table]
 
 
 def test():
-    for n in range(1, 3):
+    for n in range(1, 4):
         _, rows, *queries = open(f'./tests/{n}').read().splitlines()
         queries = [q.split() for q in queries]
         db = Database(row_counts=[int(r) for r in rows.split()])
 
         for q in queries:
             dst, src = int(q[0]), int(q[1])
-            db.merge(dst, src)
-            assert db.result == list(map(int, open(f'./tests/{n}.a').read().splitlines()))
+            db.union(dst, src)
+
+        assert db.result == list(map(int, open(f'./tests/{n}.a').read().splitlines()))
 
 
 def main():
