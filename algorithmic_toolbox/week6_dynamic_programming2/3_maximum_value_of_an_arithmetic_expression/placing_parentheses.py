fc4fa547cdf03fbf7ec51064195c92091ae09daf
kde6260 <kde6260@gmail.com>
2019-09-22 20:22:36 +0900

    course-1-week-6 solve 3. Placing Parentheses by fixing initial max, min value

--- algorithmic_toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ algorithmic_toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -32,21 +32,22 @@ def get_maximum_value(expression):
     # Bigger the k, Longer the length of sub-expression.
     for diff in range(1, amount_of_digits):
         for start in range(0, amount_of_digits - diff):
+
             end = start + diff
-            max_val = sys.maxsize
-            min_val = -sys.maxsize - 1
+            min_val = sys.maxsize
+            max_val = -sys.maxsize - 1
             for sep in range(start, end):
                 op = expression[2*sep+1]
                 a = evalt(min_arr[start][sep], op, min_arr[sep+1][end])
                 b = evalt(min_arr[start][sep], op, max_arr[sep+1][end])
                 c = evalt(max_arr[start][sep], op, max_arr[sep+1][end])
                 d = evalt(max_arr[start][sep], op, min_arr[sep+1][end])
-                max_val = max(a, b, c, d)
-                min_val = min(a, b, c, d)
+                min_val = min(min_val, a, b, c, d)
+                max_val = max(max_val, a, b, c, d)
             
             min_arr[start][end] = min_val
             max_arr[start][end] = max_val
-    
+
     return max_arr[0][amount_of_digits-1]
 
 def test1():
@@ -56,12 +57,17 @@ def test1():
 
 
 def test2():
-    result = get_maximum_value(list('5-8+7*4-8+9'))
+    result = get_maximum_value('5-8+7*4-8+9')
     print(result)
     assert result == 200
 
+def test3():
+    result = get_maximum_value('1+2-3*4-5')
+    print(result)
+    assert result == 6
 
 if __name__ == "__main__":
-    # print(get_maximum_value(input()))
-    test1()
-    test2()
+    print(get_maximum_value(input()))
+    # test1()
+    # test2()
+    # test3()
