895992fedec3a5f5ea74142f246e194861fb4740
kde6260 <kde6260@gmail.com>
2019-10-13 13:49:09 +0900

    course-2-week-6 define a function for testing

--- basic_data_structures/week4_binary_search_trees/4_set_range_sum/set_range_sum.py
+++ basic_data_structures/week4_binary_search_trees/4_set_range_sum/set_range_sum.py
@@ -1,6 +1,8 @@
 # python3
 from sys import stdin
 
+MODULO = 1000000001
+
 
 # Vertex of a splay tree
 class Vertex:
@@ -178,23 +180,50 @@ def range_sum(root, fr, to):
   merge(merged, dumped_right)
   return result
 
-MODULO = 1000000001
-n = int(stdin.readline())
-last_sum_result = 0
-for i in range(n):
-  line = stdin.readline().split()
-  if line[0] == '+':
-    x = int(line[1])
-    insert((x + last_sum_result) % MODULO)
-  elif line[0] == '-':
-    x = int(line[1])
-    delete((x + last_sum_result) % MODULO)
-  elif line[0] == '?':
-    x = int(line[1])
-    print('Found' if find((x + last_sum_result) % MODULO) else 'Not found')
-  elif line[0] == 's':
-    l = int(line[1])
-    r = int(line[2])
-    res = range_sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO)
-    print(res)
-    last_sum_result = res % MODULO
+
+def test(i):
+  n, data* = open(f'./tests/{i}').readlines()
+  result = []
+
+  for d in data:
+    op, num = d.split()
+    last_sum = 0
+    input_ = (int(num) + last_sum_result) % MODULO
+  
+    if op == '+':
+      insert(input_)
+    if op == '-':
+      delete(input_)
+    if op == '?':
+      result.append('Found' if find(input_) else 'Not Found')
+    if op == 's':
+      fr, to = list(map(lambda x: int(x), num.split()))
+      fr = (fr + last_sum_result) % MODULO
+      to = (fr + last_sum_result) % MODULO
+      result.append(range_sum((fr , to))
+
+  output* = open(f'./tests/{i}.a').readlines()
+  result = [str(r) for r in result]
+  assert result == output
+
+
+if __name__ == '__main__':
+  n = int(stdin.readline())
+  last_sum_result = 0
+  for i in range(n):
+    line = stdin.readline().split()
+    if line[0] == '+':
+      x = int(line[1])
+      insert((x + last_sum_result) % MODULO)
+    elif line[0] == '-':
+      x = int(line[1])
+      delete((x + last_sum_result) % MODULO)
+    elif line[0] == '?':
+      x = int(line[1])
+      print('Found' if find((x + last_sum_result) % MODULO) else 'Not found')
+    elif line[0] == 's':
+      l = int(line[1])
+      r = int(line[2])
+      res = range_sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO)
+      print(res)
+      last_sum_result = res % MODULO
