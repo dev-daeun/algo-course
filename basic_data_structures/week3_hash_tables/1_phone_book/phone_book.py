90ab6af956ef5e27bbc094a18e6b40de5c95ec25
kde6260 <kde6260@gmail.com>
2019-10-19 15:39:51 +0900

    course-2-week-4 write testcases

--- basic_data_structures/week3_hash_tables/1_phone_book/phone_book.py
+++ basic_data_structures/week3_hash_tables/1_phone_book/phone_book.py
@@ -3,10 +3,59 @@
 
 class Query:
     def __init__(self, query):
+        query = query.split()
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
+
+
+def test1():
+    inputs = list()
+    inputs.append(Query('add 911 police'))
+    inputs.append(Query('add 76213 Mom'))
+    inputs.append(Query('add 17239 Bob'))
+    inputs.append(Query('find 76213'))
+    inputs.append(Query('find 910'))
+    inputs.append(Query('find 911'))
+    inputs.append(Query('del 910'))
+    inputs.append(Query('del 911'))
+    inputs.append(Query('find 911'))
+    inputs.append(Query('find 76213'))
+    inputs.append(Query('add 76213 Dad'))
+    inputs.append(Query('add 76213'))
+    
+    result = process_queries(inputs)
+
+    assert result == [
+        'Mom',
+        'not found',
+        'police',
+        'not found',
+        'Mom',
+        'Dad',
+    ]
+
+
+def test2():
+    inputs = list()
+    inputs.append(Query('find 3839442'))
+    inputs.append(Query('add 123456 me'))
+    inputs.append(Query('add 0 granny'))
+    inputs.append(Query('find 0'))
+    inputs.append(Query('find 123456'))
+    inputs.append(Query('del 0'))
+    inputs.append(Query('del 0'))
+    inputs.append(Query('find 0'))
+
+    result = process_queries(inputs)
+
+    assert result == [
+        'not found',
+        'granny',
+        'me',
+        'not found',
+    ]
 
 
 def read_queries():
