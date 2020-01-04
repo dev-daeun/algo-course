e3d114bc5f6016e5fdabd852d8dc64fc5fd52f7f
kde6260 <kde6260@gmail.com>
2020-01-04 16:26:37 +0900

    solve 1.Acyclicity & add test function

--- algorithm_on_graphs/week2_decomposition2/1_acyclicity/acyclicity.py
+++ algorithm_on_graphs/week2_decomposition2/1_acyclicity/acyclicity.py
@@ -28,6 +28,30 @@ def is_cyclic(v):
     return False
 
 
+def test(num):
+    global n
+    global m
+    global pre_time
+    global post_time
+    global adjacent_list
+
+    with open('tests/{}'.format(num)) as input_:
+        n, m = list(map(int, input_.readline().split(' ')))
+        pre_time = [None for _ in range(n)]
+        post_time = [None for _ in range(n)]
+        adjacent_list = [list() for _ in range(n)]
+
+        for _ in range(m):
+            v1, v2 = list(map(int, input_.readline().split(' ')))
+            adjacent_list[v1-1].append(v2 - 1)
+
+        result = dfs()
+
+    with open('tests/{}.a'.format(num)) as output:
+        expected_result = int(output.readline())
+        assert result == expected_result
+
+
 if __name__ == '__main__':
     n, m = list(map(int, stdin.readline().split(' ')))
     pre_time = [None for _ in range(n)]
@@ -40,3 +64,5 @@ if __name__ == '__main__':
 
     print(1) if dfs() else print(0)
 
+    # test(1)
+    # test(2)
