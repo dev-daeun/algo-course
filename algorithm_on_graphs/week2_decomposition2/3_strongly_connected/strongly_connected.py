c8dfe4ca473ecb62b2bef4221782ed99e01ce176
kde6260 <kde6260@gmail.com>
2020-01-05 13:08:16 +0900

    solve 3.Strongly Connected

--- algorithm_on_graphs/week2_decomposition2/3_strongly_connected/strongly_connected.py
+++ algorithm_on_graphs/week2_decomposition2/3_strongly_connected/strongly_connected.py
@@ -1,9 +1,6 @@
 # Uses python3
 
-from sys import stdin, setrecursionlimit
-
-
-setrecursionlimit(200000)
+from sys import stdin
 
 
 def number_of_strongly_connected_components():
@@ -25,21 +22,78 @@ def topological_sort_reverse():
     return [x[0] for x in sorted_result]
 
 
-def explore_reversed_graph(v):
-    for adj_v in reverse_adjacent_list[v]:
-        if not post_time[adj_v]:
-            explore_reversed_graph(adj_v)
+def explore_reversed_graph(init_v):
+    stack = list()
+    stack.append(init_v)
 
-    global post_time_cnt
-    post_time_cnt += 1
-    post_time[v] = (v, post_time_cnt)
+    while stack:
+        v = stack[-1]
+        is_pushed = False
+        for adj_v in reverse_adjacent_list[v]:
+            if not pre_time[adj_v]:
+                global pre_time_cnt
+                pre_time_cnt += 1
+                pre_time[adj_v] = pre_time_cnt
+
+                stack.append(adj_v)
+                is_pushed = True
+                break
+
+        if not is_pushed:
+            complete_v = stack.pop(-1)
+            global post_time_cnt
+            post_time_cnt += 1
+            post_time[complete_v] = (complete_v, post_time_cnt)
 
 
-def explore(v):
-    visited[v] = True
-    for adj_v in adjacent_list[v]:
-        if not visited[adj_v]:
-            explore(adj_v)
+def explore(init_v):
+    visited[init_v] = True
+
+    stack = list()
+    stack.append(init_v)
+
+    while stack:
+        v = stack[-1]
+        is_pushed = False
+
+        for adj_v in adjacent_list[v]:
+            if not visited[adj_v]:
+                visited[adj_v] = True
+                stack.append(adj_v)
+                is_pushed = True
+                break
+
+        if not is_pushed:
+            stack.pop(-1)
+
+
+def test(num):
+    global n, m, adjacent_list, reverse_adjacent_list, post_time, post_time_cnt, visited, pre_time, pre_time_cnt
+
+    with open('tests/{}'.format(num)) as input_:
+        n, m = list(map(int, input_.readline().split(' ')))
+
+        adjacent_list = [list() for _ in range(n)]
+        reverse_adjacent_list = [list() for _ in range(n)]
+
+        for _ in range(m):
+            v1, v2 = list(map(int, input_.readline().split(' ')))
+            adjacent_list[v1 - 1].append(v2 - 1)
+            reverse_adjacent_list[v2 - 1].append(v1 - 1)
+
+        pre_time = [None for _ in range(n)]
+        post_time = [None for _ in range(n)]
+        pre_time_cnt = 0
+        post_time_cnt = 0
+
+        visited = [False for _ in range(n)]
+
+        result = number_of_strongly_connected_components()
+
+    with open('tests/{}.a'.format(num)) as output:
+        expected_result = int(output.readline().strip('\n'))
+
+        assert result == expected_result
 
 
 if __name__ == '__main__':
@@ -53,9 +107,14 @@ if __name__ == '__main__':
         adjacent_list[v1-1].append(v2 - 1)
         reverse_adjacent_list[v2-1].append(v1 - 1)
 
+    pre_time = [None for _ in range(n)]
+    pre_time_cnt = 0
     post_time = [None for _ in range(n)]
     post_time_cnt = 0
 
     visited = [False for _ in range(n)]
 
     print(number_of_strongly_connected_components())
+    # test(1)
+    # test(2)
+    # test(3)
