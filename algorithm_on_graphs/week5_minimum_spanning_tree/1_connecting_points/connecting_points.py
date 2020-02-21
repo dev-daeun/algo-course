7100b1dbe0ca81318cf53d64dbf2fc7c3633711a
kde6260 <kde6260@gmail.com>
2020-02-22 01:10:06 +0900

    define DisjointSet class with find() & implement Kruskal algorithm

--- algorithm_on_graphs/week5_minimum_spanning_tree/1_connecting_points/connecting_points.py
+++ algorithm_on_graphs/week5_minimum_spanning_tree/1_connecting_points/connecting_points.py
@@ -9,29 +9,53 @@ class City:
         self.y = y
 
 
+class DisjointSet:
+    def __init__(self, vertexes, n):
+        self.parents = list(range(n))
+        self.vertexes = vertexes
+        self.ranks = [0 for _ in range(n)]
+
+    def find(self, e):
+        p = e
+        while self.parents[p] != p:
+            p = self.parents[p]
+        self.parents[e] = p
+        return p
+
+    def union(self, v1, v2):
+        pass
+
+
 def distance(city1, city2):
     return pow(pow(city1.x - city2.x, 2) + pow(city1.y - city2.y, 2), 0.5)
 
 
-def minimum_distance(vertexes, edges):
-    result = 0.
-    # write your code here
-    return result
+def kruskal(n, vertexes, dist):
+    sorted_dist = sorted(dist, key=lambda e: e[2])
+    disjoint_set = DisjointSet(vertexes, n)
+    sum_of_edges = 0.
+
+    for v1, v2, d in sorted_dist:
+        if disjoint_set.find(v1) != disjoint_set.find(v2):
+            disjoint_set.union(v1, v2)
+            sum_of_edges += d
+
+    return sum_of_edges
 
 
 if __name__ == '__main__':
     number_of_v = int(stdin.readline().strip('\n'))
     vertexes = list()
-    edges = list()
+    dist = list()
+
     for _ in range(number_of_v):
         x, y = list(map(int, stdin.readline().split(' ')))
         v = City(x, y)
 
-        for c in vertexes:
-            edges.append((c, v, distance(c, v)))
+        for idx, prev_v in enumerate(vertexes):
+            dist.append((idx, len(vertexes), distance(prev_v, v)))
+
         vertexes.append(v)
 
-    sorted_edges = sorted(edges, key=lambda e: e[2])
-    print("{0:.9f}".format(minimum_distance(vertexes, sorted_edges)))
-
+    print("{0:.9f}".format(kruskal(number_of_v, vertexes, dist)))
 
